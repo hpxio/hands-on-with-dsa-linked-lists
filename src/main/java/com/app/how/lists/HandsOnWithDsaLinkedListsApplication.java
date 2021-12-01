efad8abcd28c8ede972df7fc8f4eb8badcfdf8b6
HPxIO <hpx.io@keemail.me>
2021-12-01 15:40:32 +0530

    initial commit

---
+++ src/main/java/com/app/how/lists/HandsOnWithDsaLinkedListsApplication.java
@@ -0,0 +1,16 @@
+package com.app.how.lists;
+
+import org.springframework.boot.CommandLineRunner;
+
+public class HandsOnWithDsaLinkedListsApplication implements CommandLineRunner {
+
+    public static void main(String[] args) {
+    }
+
+
+
+    @Override
+    public void run(String... args) throws Exception {
+
+    }
+}
---
+++ src/main/java/com/app/how/lists/sll/SinglyLinkedList.java
@@ -0,0 +1,20 @@
+package com.app.how.lists.sll;
+
+public class SinglyLinkedList {
+
+    /* Linked-List Node (simple string) */
+    class Node {
+        String data;
+        Node next;
+
+        Node() {
+            data = null;
+            next = null;
+        }
+    }
+
+    /* Initially List is empty */
+    Node head = null;
+
+    /* Insert new element in the list */
+}
